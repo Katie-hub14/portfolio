ae35a7dad59b16b698a52c57397aea11c9d9f64a
Katie <655044@pdsb.net>
2021-03-19 11:33:18 -0400

    moved files

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function f1() {​​​​​
+alert("Coming soon!");
+}​​​​​
+function f2() {​​​​​
+alert("Coming soon!");
+}​​​​​
+function f3() {​​​​​
+alert("Coming soon!");
+}​​​​​
--- styles/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function f1() {​​​​​
-alert("Coming soon!");
-}​​​​​
-function f2() {​​​​​
-alert("Coming soon!");
-}​​​​​
-function f3() {​​​​​
-alert("Coming soon!");
-}​​​​​
