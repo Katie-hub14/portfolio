2486452524cabd7221c9c6eefc746695774c35f9
Katie <655044@pdsb.net>
2021-04-23 11:56:02 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,12 @@ alert("Coming soon!");
 function f3() {​​​​​
 alert("Coming soon!");
 }​​​​​
-//encryption(encrypts a plain text into a cipher text)
+//encryption(encrypts the plain text into a cipher text)
 function encrypt() {
     var pmessage = prompt ("Enter the plaintext message here");
     var ciphertext = "";
     ciphertext = pmessage.replace(/a/g , "at").replace(/k/g , "kat").replace(/e/g , "et");
-    document.getElementById("labox").innerhtml = ciphertext.charAt()
+    document.getElementById("labox").innerhtml = ciphertext.charCodeAt()
 }
 
 //decryption (decrypts the cipher text into the plain text)
@@ -20,4 +20,4 @@ function decrypt()
    var cmessage = prompt("Enter a ciphertext message");
    var plaintext = "";
    plaintext = cmessage.replace(/kat/g , "k").replace(/et/g , "e").replace(/at/g , "a");
-   document.getElementById("labox").innerHTML = plaintext.charAt()
+   document.getElementById("labox").innerHTML = plaintext.charCodeAt()
