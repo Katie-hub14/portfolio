c6a335f6835d64e764f6488a903c77e2f9acb384
Katie <655044@pdsb.net>
2021-04-23 10:46:32 -0400

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,17 @@ alert("Coming soon!");
 function f3() {​​​​​
 alert("Coming soon!");
 }​​​​​
+//encryption(encrypts a plain text into a cipher text)
+function encrypt() {
+    var pmessage = prompt ("Enter the plaintext message here");
+    var ciphertext = "";
+    ciphertext = pmessage.replace(/a/g , "at").replace(/k/g , "kat").replace(/e/g , "et");
+    document.getElementById("box").innerhtml = ciphertext.charAt()
+}
+
+//decryption (decrypts the cipher text into the plain text)
+function decrypt()
+   var cmessage = prompt("Enter a ciphertext message");
+   var plaintext = "";
+   plaintext = cmessage.replace(/kat/g , "k").replace(/et/g , "e").replace(/at/g , "a");
+   document.getElementById("box").innerHTML = plaintext.charCodeAt()
