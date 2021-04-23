04a1bb1b992a202d89d4e9b7b9f7f0a4022ea129
Katie <655044@pdsb.net>
2021-04-23 11:57:48 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ alert("Coming soon!");
 }​​​​​
 //encryption(encrypts the plain text into a cipher text)
 function encrypt() {
-    var pmessage = prompt ("Enter the plaintext message here");
+    var pmessage =prompt ("Enter the plaintext message here");
     var ciphertext = "";
     ciphertext = pmessage.replace(/a/g , "at").replace(/k/g , "kat").replace(/e/g , "et");
     document.getElementById("labox").innerhtml = ciphertext.charCodeAt()
@@ -17,7 +17,7 @@ function encrypt() {
 
 //decryption (decrypts the cipher text into the plain text)
 function decrypt()
-   var cmessage = prompt("Enter a ciphertext message");
+   var cmessage =prompt("Enter a ciphertext message");
    var plaintext = "";
    plaintext = cmessage.replace(/kat/g , "k").replace(/et/g , "e").replace(/at/g , "a");
    document.getElementById("labox").innerHTML = plaintext.charCodeAt()
